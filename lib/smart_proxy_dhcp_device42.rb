ed89d724f0254224085f5cf400233251f06ab712
Anatolii Chmykhalo <tolya.double@gmail.com>
2018-02-17 00:02:49 +0300

    module_loader deps

--- lib/smart_proxy_dhcp_device42.rb
+++ lib/smart_proxy_dhcp_device42.rb
@@ -7,5 +7,6 @@ end
 require 'smart_proxy_dhcp_device42/plugin_configuration'
 require 'smart_proxy_dhcp_device42/scheme_validator'
 require 'smart_proxy_dhcp_device42/verify_validator'
+require 'smart_proxy_dhcp_device42/module_loader'
 require 'smart_proxy_dhcp_device42/dhcp_device42_version'
 require 'smart_proxy_dhcp_device42/dhcp_device42_plugin'
